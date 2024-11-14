ecf12318bc30fe7a196acff4e84f3535c9d2dd89
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-14 15:11:42 +0200

    add-new-sities

--- Project-16/Capital.swift
+++ Project-16/Capital.swift
@@ -8,14 +8,56 @@
 import MapKit
 import UIKit
 
-class Capital: NSObject, MKAnnotation {
+// Capital теперь является классом, так как MKAnnotation требует класса
+class Capital: NSObject, MKAnnotation, Decodable {
+    
+    var id: Int
     var title: String?
-    var coordinate: CLLocationCoordinate2D
     var info: String
+    var coordinate: CLLocationCoordinate2D
 
-    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+    // Инициализатор для класса
+    init(id: Int, title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.id = id
         self.title = title
         self.coordinate = coordinate
         self.info = info
     }
+    
+    // Структура для координат, которая соответствует Decodable
+    struct Coordinate: Decodable {
+        var latitude: Double
+        var longitude: Double
+        
+        enum CodingKeys: String, CodingKey {
+            case latitude
+            case longitude
+        }
+        
+        // Инициализатор для преобразования в CLLocationCoordinate2D
+        func toCLLocationCoordinate2D() -> CLLocationCoordinate2D {
+            return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        }
+    }
+    
+    // Для декодирования
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        info = try container.decode(String.self, forKey: .info)
+        
+        // Декодируем координаты
+        let coordinateData = try container.decode(Coordinate.self, forKey: .coordinate)
+        coordinate = coordinateData.toCLLocationCoordinate2D()  // Преобразуем в CLLocationCoordinate2D
+    }
+    
+    // CodingKeys для декодирования
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case coordinate
+        case info
+    }
 }
+
--- Project-16/ViewController.swift
+++ Project-16/ViewController.swift
@@ -12,22 +12,28 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet var mapView: MKMapView!
     
+    var cities = [Capital]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         showMapTypeSelection()
         
-        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
-        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
-        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
-        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
-        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
+        loadSity()
         
-        mapView.addAnnotation(london)
-        mapView.addAnnotation(oslo)
-        mapView.addAnnotation(paris)
-        mapView.addAnnotation(rome)
-        mapView.addAnnotation(washington)
+        /* альтернатива, без json
+         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
+         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+         let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
+         let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
+         
+         mapView.addAnnotation(london)
+         mapView.addAnnotation(oslo)
+         mapView.addAnnotation(paris)
+         mapView.addAnnotation(rome)
+         mapView.addAnnotation(washington)
+         */
         
         // mapView.addAnnotations([london, oslo, paris, rome, washington]) // альтернатива
     }
@@ -72,33 +78,64 @@ class ViewController: UIViewController, MKMapViewDelegate {
         vc.infoCountry = capital.title
         navigationController?.pushViewController(vc, animated: true)
         
-//        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
-//        ac.addAction(UIAlertAction(title: "OK", style: .default))
-//        present(ac, animated: true)
+        //        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        //        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        //        present(ac, animated: true)
     }
     
     func showMapTypeSelection() {
-          // Создаём UIAlertController с вариантами выбора типа карты
-          let alert = UIAlertController(title: "Choose Map Type", message: "Select the map type you prefer", preferredStyle: .actionSheet)
-          
-          // Добавляем действия для разных типов карт
-          alert.addAction(UIAlertAction(title: "Standard", style: .default, handler: { _ in
-              self.mapView.mapType = .standard
-          }))
-          
-          alert.addAction(UIAlertAction(title: "Satellite", style: .default, handler: { _ in
-              self.mapView.mapType = .satellite
-          }))
-          
-          alert.addAction(UIAlertAction(title: "Hybrid", style: .default, handler: { _ in
-              self.mapView.mapType = .hybrid
-          }))
-          
-          alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-          
-          // Показываем UIAlertController
-          present(alert, animated: true, completion: nil)
-      }
+        // Создаём UIAlertController с вариантами выбора типа карты
+        let alert = UIAlertController(title: "Choose Map Type", message: "Select the map type you prefer", preferredStyle: .actionSheet)
+        
+        // Добавляем действия для разных типов карт
+        alert.addAction(UIAlertAction(title: "Standard", style: .default, handler: { _ in
+            self.mapView.mapType = .standard
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Satellite", style: .default, handler: { _ in
+            self.mapView.mapType = .satellite
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Hybrid", style: .default, handler: { _ in
+            self.mapView.mapType = .hybrid
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        // Показываем UIAlertController
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func loadSity() {
+        
+        DispatchQueue.global().async {
+            // Получаем путь к файлу в bundle приложения
+            if let url = Bundle.main.url(forResource: "localCountries", withExtension: "json") {
+                
+                do {
+                    // Читаем данные из файла
+                    let data = try Data(contentsOf: url)
+                    
+                    // Декодируем данные в массив объектов типа Capital
+                    let decoder = JSONDecoder()
+                    let cities = try decoder.decode([Capital].self, from: data)
+                    
+                  // Печатаем загруженные данные в консоль для проверки
+//                    print("Loaded items: \(cities[0].title)") // Выводим первый объект как пример
+                    
+                    // Переходим на главный поток для обновления UI
+                    DispatchQueue.main.async { [weak self] in
+                        // Добавляем аннотации на карту
+                        self?.mapView.addAnnotations(cities)
+                    }
+                    
+                } catch {
+                    // Обработка ошибок
+                    print("Error loading or decoding JSON: \(error)")
+                }
+            }
+        }
+    }
     
 }
 
